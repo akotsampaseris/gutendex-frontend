b9452908c45f6404f33f892d185fb6b4cf711edb
Antony Kotsampaseris <a.kotsampaseris@gmail.com>
2022-09-09 06:55:06 +0300

    Deploy to Vercel

--- pages/books/book/[bookID].jsx
+++ pages/books/book/[bookID].jsx
@@ -3,10 +3,12 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react';
 
-export default function Book() {
-  const [book, setBook] = useState('');
+export default function BookView() {
   const router = useRouter();
   const { bookID } = router.query;
+
+  const [book, setBook] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchBook() {
@@ -15,22 +17,108 @@ export default function Book() {
       setBook(json);
     }
     fetchBook();
-  }, [])
+    setLoading(false);
+  })
 
 
   return (
     <div className="container">
       <Head>
-        <title>{book.title} | {book.author}</title>
+        <title>{book.title} | GutendexAPI</title>
         <meta name="description" content="NextJS app using Gutendex API" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="">
-        <h2 className="">{book.title}</h2>
-        <Link href="/">Go back</Link>
+        <div className="grid grid-cols-5 gap-2 border-b-2 border-indigo-500 w-full p-4">
+          <div>
+            <h1 className="text-2xl text-indigo-500">
+              <Link href="/">Search GutendexAPI</Link>
+            </h1>
+          </div>
+        </div>
+        <div className="w-8/12 mx-auto m-4 p-10 border border-indigo-500 rounded">
+          <BookDetails book={book} loading={loading} />
+        </div>
       </main>
     </div>
   )
 }
 
+function BookDetails({book, loading}){
+  if (loading || !book.authors) return (
+    <p className="text-small text-center text-indigo-500">Loading...</p>
+  )
+  
+  
+  return (
+    <div>
+      <div className="flex justify-between gap-4">
+        <Link href={{pathname: `/books/book/${book.id}`}}>
+          <span className="text-xl text-indigo-500">{book.title}</span>
+        </Link>
+        <div className="flex-col">
+          <p className="text-lg text-blue-500">Rating: 5/5</p>
+          <p className="text-xs text-blue-300 text-center">
+            <Link href="/">View all ratings</Link>
+          </p>
+        </div>
+      </div>
+      <div className="flex">
+        <span className="text-md">Author(s): </span> 
+        <ul className="list-none">
+        {book.authors.map(author => {
+          return (<li className="pl-2" key={author.name}>{author.name}</li>)
+        })}
+        </ul>
+      </div>
+      <div className="text-xs text-indigo-500 pt-1">
+        <span>Languages: {book.languages}</span> 
+        <span> | </span> 
+        <span>Downloads: {book.download_count}</span>
+      </div>
+      <PostBookReview book={book} />
+    </div>
+  )
+}
+
+function PostBookReview({book}){
+  return (
+    <div className="mt-5 pt-5">
+      <p className="text-xl text-blue-400">Your review for "{book.title}".</p>
+      <form className="pt-3">
+        <label className="block flex pb-2">
+          <span>Rating: </span>
+          <select className="rounded bg-white text-black ml-2">       
+            <option value="5">5</option>
+            <option value="4">4</option>
+            <option value="3">3</option>
+            <option value="2">2</option>
+            <option value="1">1</option>
+          </select>
+        </label>
+        <textarea className="
+          form-control
+          block
+          w-full
+          pt-4
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+        " placeholder="Write your review here..." id="review" name="review" rows="4" cols="50" />
+        <button className="mt-2 py-2 px-4 rounded bg-indigo-400 hover:bg-indigo-600">
+          Post your review
+        </button>
+      </form>
+    </div>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,51 +1,106 @@
 import Head from 'next/head'
 import Link from 'next/link';
-import styles from '../styles/Home.module.css'
-import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
-
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [searchString, setSearchString] = useState('');
   const router = useRouter();
+  const { title } = router.query;
+
+  const [loading, setLoading] = useState(false);
+  const [searchString, setSearchString] = useState('');
+  const [books, setBooks] = useState([]);
+
+  async function fetchData(){
+    try{
+      const res = await fetch(`http://localhost:8000/books/search?title=${searchString}`)
+      const json = await res.json();
+      setBooks(json);
+      setLoading(false);
+    } catch(e) {
+      console.log(e)
+    }
+  }
 
   function handleChange(e){
     setSearchString(e.target.value);
   }
 
   const handleSubmit = e => {
+    setLoading(false);
     e.preventDefault();
-    router.push("/books/search-results")
+    setLoading(true);
+    fetchData();
   }
 
 
   return (
     <div className="container-md mx-auto px-4">
       <Head>
-        <title>Search Gutendex | A. Kotsampaseris</title>
-        <meta name="description" content="NextJS app using Gutendex API" />
+        <title>Search Results | GutendexAPI</title>
+        <meta name="description" content="NextJS app using GutendexAPI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-screen w-full mx-auto p-5">
-        <div className="w-8/12 flex flex-col justify-items-center">
-          <h1 className="text-2xl text-center">
-            <Link href="/">Search GutendexAPI</Link>
-          </h1>
-          <form className="w-full pt-4 flex gap-2" onSubmit={handleSubmit}>
-            <input
-              placeholder='eg Frankenstein'
-              type='text'
-              className='mt-1 block w-9/12 px-3 py-2 bg-white border border-slate-300 rounded-md text-black text-sm shadow-sm placeholder-slate-400
-              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
-              value={searchString}
-              onChange={handleChange}
-            />
-            <button type="submit" className="w-3/12 rounded bg-sky-500 hover:bg-sky-700">
-                Search
-            </button>
-          </form>
+      <main className="w-full mt-2 m-0">
+        <div className="grid grid-cols-5 gap-2 border-b-2 border-indigo-500 w-full p-4">
+          <div>
+            <h1 className="text-2xl text-indigo-500">
+              <Link href="/">Search GutendexAPI</Link>
+            </h1>
+          </div>
+          <div className="col-span-4">
+            <form className="flex gap-2" onSubmit={handleSubmit}>
+              <input
+                placeholder='Moby Dick, Frankenstein etc'
+                type='text'
+                className='mt-1 block w-9/12 px-3 py-2 bg-white border border-slate-300 rounded-md text-black text-sm shadow-sm placeholder-slate-400
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+                value={searchString}
+                onChange={handleChange}
+              />
+              <button type="submit" className="w-3/12 rounded bg-indigo-400 hover:bg-indigo-600">
+                  Search
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="mx-auto m-4 p-4 w-8/12">
+          <p className="text-2xl text-center">Results</p>
+          <BookList books={books} loading={loading} />
         </div>
       </main>
+    </div>
+  )
+}
+
+function BookList({books, loading}){
+  if (loading) return (
+    <p className="text-small text-center text-indigo-500">Results loading...</p>
+  )
+
+  if (!loading && !books.length) return (
+    <p className="text-small text-center text-indigo-500">Search for a book you love.</p>
+  )
+
+  return (
+    <div>
+      {books.map(book => (
+        <div className="m-4 p-4 border border-indigo-500 rounded" key={book.id}>
+          <div className="text-xl text-indigo-500">
+            <Link href={{pathname: `/books/book/${book.id}`}}>{book.title}</Link>
+          </div>
+          <div className="flex">
+            <p>Author(s): </p> 
+            <ul className="list-none">
+            {book.authors.map(author => {
+              return (<li className="pl-2" key={author.name}>{author.name}</li>)
+            })} 
+            </ul>
+          </div>
+          <div className="text-xs text-indigo-500">Downloads: {book.download_count}</div>
+        </div>
+        )
+      )}
     </div>
   )
 }
